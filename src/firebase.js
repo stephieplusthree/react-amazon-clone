08c4453c16d373d0232ace697301b1bd28db63e5
stephanie <stephieplusthree@gmail.com>
2020-10-24 14:42:09 -0700

    router added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDslS_DTwfvW_TDCp7eVcbRLDwFyvGVyT8",
+    authDomain: "clone-68544.firebaseapp.com",
+    databaseURL: "https://clone-68544.firebaseio.com",
+    projectId: "clone-68544",
+    storageBucket: "clone-68544.appspot.com",
+    messagingSenderId: "350943376353",
+    appId: "1:350943376353:web:5d09e26ad7caa4472ed8a0",
+    measurementId: "G-LLQSLPDK68"
+};
