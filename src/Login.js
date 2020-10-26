456734cb58fe5e5c927af89343ffe405babb7ce4
stephanie <stephieplusthree@gmail.com>
2020-10-26 13:44:23 -0700

    front-end login added

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from "react-router-dom";
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className="login">
             <Link to="/">
@@ -17,10 +20,10 @@ function Login() {
 
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e => setPassword(e.tartget.value)} />
 
                     <button className="login__signInButton">Sign In</button>
                 </form>
