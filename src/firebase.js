3b6b1ce76edae31db07a7005694f6a942fab942e
stephanie <stephieplusthree@gmail.com>
2020-10-26 22:01:01 -0700

    register added

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDslS_DTwfvW_TDCp7eVcbRLDwFyvGVyT8",
     authDomain: "clone-68544.firebaseapp.com",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     appId: "1:350943376353:web:5d09e26ad7caa4472ed8a0",
     measurementId: "G-LLQSLPDK68"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
