d56e1b9aa2e05f52b0ed51a723e871b913f13018
stephanie <stephieplusthree@gmail.com>
2020-10-27 10:53:20 -0700

    login

--- src/Login.js
+++ src/Login.js
@@ -40,6 +40,7 @@ function Login() {
       <Link to="/">
         <img
           className="login__logo"
+          alt="amazon logo"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
         />
       </Link>
